e2bba94cee98dbdf19c8a6da2b1d657fca5fdcbf
rajmytaj <chalasanidheeraj@gmail.com>
2017-08-04 16:35:02 +0000

    finished users feature incl. login/logout and registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+
+    user = User.new(user_params)
+
+      if user.save
+        session[:user_id] = user.id
+        redirect_to '/'
+      else
+        redirect_to '/users/new'
+      end
+    
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
 
+  resources :users, only: [:create, :new]
   root to: 'products#index'
+
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  resource :session, except: [:edit, :update, :show]
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:index, :show]
@@ -18,6 +29,8 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
     
   end
+
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
