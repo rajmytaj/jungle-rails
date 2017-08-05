9ee8ddaa3d24e20680c6c1486488140d98117b39
rajmytaj <chalasanidheeraj@gmail.com>
2017-08-05 00:36:08 +0000

    admin authentication feature fin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CategoriesController < ApplicationController
-
+  http_basic_authenticate_with name: ENV["ADMIN_USER"], password:ENV["ADMIN_PASS"]
   def new 
     @category = Category.new
   end
