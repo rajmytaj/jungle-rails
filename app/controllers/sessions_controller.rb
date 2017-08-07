5876876b5ee893504acc7e7decfd79947f2c4565
rajmytaj <chalasanidheeraj@gmail.com>
2017-08-07 18:44:15 +0000

    fixed login error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,24 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(session_params[:email])
-    if user && user.authenticate(session_params[:password])
+    user = User.find_by_email(params[:email])
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(params[:password])
+      # Save the user id inside the browser cookie. This is how we keep the user 
+      # logged in when they navigate around our website.
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/login', flash: { error: 'Invalid user email or password.' }
+    # If user's login doesn't work, send them back to the login form.
+      redirect_to '/login'
     end
-    
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to '/login'
-  end
-
-  private
-  
-  def session_params
-    params.require(:user).permit(:email, :password)
   end
 end
