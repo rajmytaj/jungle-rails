32d5696cf0414e1fd04ad4fca74a1abca79c6e72
rajmytaj <chalasanidheeraj@gmail.com>
2017-08-07 22:10:25 +0000

    fixed route errors, reviews show up, and new seeds added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,25 +1,25 @@
 class ReviewsController < ApplicationController
-
-  def create 
-    @product     = Product.find params[:product_id]
-    @review      = @product.reviews.create review_params
-    @review.user = current_user
-    if @review.save
-      redirect_to product_path(@product), notice: 'Your review has been posted!'
-    else
-      redirect_to product_path(@product), flash: { error: 'Failed to create review' }
+    before_filter :authorize, :except => [:show]
+    def create
+        @product = Product.find(params[:product_id])
+        @review = @product.reviews.create(review_params)
+        @review.user = current_user
+      if @review.save
+        redirect_to @product, notice: 'Review was successfully created.'
+      else
+        redirect_to @product, notice: 'Review failed to save.'
+      end
     end
-  end
 
-  def destroy
-    @review = Review.find params[:id]
-    @review.destroy!
-    redirect_to @review.product, flash: { error: 'Review deleted!' }
-  end
+    def destroy
+        @review = Review.find(params[:id])
+        @review.destroy
+        redirect_to @review.product, notice: 'Review was successfully deleted.'
+    end
 
-  private
+    private
 
-  def review_params
-    params.require(:review).permit(:description, :rating)
-  end
+    def review_params
+        params.require(:review).permit(:description, :rating)
+    end
 end
